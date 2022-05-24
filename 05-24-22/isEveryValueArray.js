56452edc02dbdb710379fb955652a855a2e00d60
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-05-24 08:44:00 -0400

    completed daily code challenges

---
+++ 05-24-22/isEveryValueArray.js
@@ -0,0 +1,9 @@
+// Instructions
+
+/* Is every value in the array an array?
+
+This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.*/
+
+const arrCheck = value => value.every(Array.isArray) ? true : false
+
+// I did not pass this challenge. I knew how to solve it but I did not stop to do pseudo code and focus on the directions to realize what it was asking
