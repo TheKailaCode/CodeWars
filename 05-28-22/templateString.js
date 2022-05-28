1d98ae53ba37e23868a76a06ba4e39e426e48872
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-05-28 10:59:16 -0400

    completed daily codewars challenge

---
+++ 05-28-22/templateString.js
@@ -0,0 +1,8 @@
+// Instructions
+
+// Your task is to return the correct string using the Template String Feature.
+
+var TempleStrings = function (obj, feature) {
+
+    return `${obj} are ${feature}`
+}
