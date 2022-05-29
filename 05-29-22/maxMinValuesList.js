1581eaea10ee1cfc52b0f8330d9275e89a2a699c
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-05-29 11:15:29 -0400

    completed daily code challenge

---
+++ 05-29-22/maxMinValuesList.js
@@ -0,0 +1,13 @@
+// Instructions
+
+// Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+
+var min = function (list) {
+    let minValue = Math.min(...list)
+    return minValue;
+}
+
+var max = function (list) {
+    let maxValue = Math.max(...list)
+    return maxValue;
+}
