c3e7a236c0d4dda65b8e38efd4f851bcee63a568
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-06-29 10:39:27 -0400

    completed daily code challenge

---
+++ 06-29-22/highestProfitWins.js
@@ -0,0 +1,7 @@
+// Instructions
+
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+function minMax(arr) {
+    return [Math.min(...arr), Math.max(...arr)]
+}
