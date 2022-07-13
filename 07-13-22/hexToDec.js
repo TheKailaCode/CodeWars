574756c7842f307b2aeb99e81d5e2251b47f4670
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-07-13 13:18:41 -0400

    completed daily code challenge

---
+++ 07-13-22/hexToDec.js
@@ -0,0 +1,7 @@
+// Instructions
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+function hexToDec(hexString) {
+    return parseInt(hexString, 16)
+}
