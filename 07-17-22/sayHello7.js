246d443d62e702a190113e29e19d7df28435ad1d
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-07-17 17:27:19 -0400

    completed daily code challenge

---
+++ 07-17-22/sayHello7.js
@@ -0,0 +1,11 @@
+// Instructions
+
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+
+function greet(name) {
+    if (name == null || name == '') {
+        return null
+    } else {
+        return `hello ${name}!`
+    }
+}
