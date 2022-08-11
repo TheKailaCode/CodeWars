5555ff6cde0b0ba63f6501dec2592513382b8864
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-08-11 09:32:03 -0400

    Keep Hydrated challenge

--- challenges/main.js
+++ challenges/main.js
@@ -389,4 +389,11 @@ function fourSeven(n) {
     idealResult = 28 / n
     return idealResult || falseResult || negatives
 }
-fourSeven(4)
+// fourSeven(4)
+
+
+// Keep Hydrated!
+
+function litres(time) {
+    return Math.floor(time * .5);
+}
