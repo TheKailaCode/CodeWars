2cabb600b5dd967fbb24eb8ba27f42b148da2878
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-08-16 12:49:20 -0400

    Fake Binary challenge

--- challenges/main.js
+++ challenges/main.js
@@ -491,3 +491,18 @@ function areYouPlayingBanjo(name) {
     }
 }
 // areYouPlayingBanjo('Ringo')
+
+
+// Fake Binary
+
+let fakeBin = x => {
+    let newStr = ''
+    for (let i = 0; i <= x.length; i++) {
+        if (x[i] < 5) {
+            newStr += '0'
+        } else if (x[i] >= 5) {
+            newStr += '1'
+        }
+    }
+    return newStr
+}
